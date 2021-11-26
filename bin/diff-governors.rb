b6159c2280ab1320297ed3e0fd0af77cb0a2f975
Tony Bowden <tony@tmtm.com>
2021-11-26 18:54:19 +0200

    Add scrapers for Regional Governors and Chief Ministers

---
+++ bin/diff-governors.rb
@@ -0,0 +1,17 @@
+#!/bin/env ruby
+# frozen_string_literal: true
+
+require 'every_politician_scraper/comparison'
+require 'every_politician_scraper/scraper_data'
+require 'pry'
+
+# Only compare IDs, not names, as those differ between WP/WD
+class Comparison < EveryPoliticianScraper::Comparison
+  def columns
+    super
+    # super.reject { |name| name.to_s.downcase.include? 'label' }
+  end
+end
+
+diff = Comparison.new('wikidata/results/governors.csv', 'data/governors-wikipedia.csv').diff
+puts diff.sort_by { |r| [r.first.to_s, r[1].to_s] }.reverse.map(&:to_csv)
---
+++ wikidata/queries/governors.js
@@ -0,0 +1,17 @@
+module.exports = function () {
+  return `SELECT DISTINCT ?region ?regionLabel ?position ?positionLabel ?person ?personLabel ?start
+         (STRAFTER(STR(?ps), '/statement/') AS ?psid)
+  WHERE {
+    ?region wdt:P31|wdt:P31/wdt:P279 wd:Q270496 ; wdt:P31/wdt:P279* wd:Q10864048 ; wdt:P1906 ?position .
+    MINUS { ?region wdt:P576 [] }
+
+    OPTIONAL {
+      ?person wdt:P31 wd:Q5 ; p:P39 ?ps .
+      ?ps ps:P39 ?position ; pq:P580 ?start .
+      FILTER NOT EXISTS { ?ps pq:P582 ?end }
+    }
+
+    SERVICE wikibase:label { bd:serviceParam wikibase:language "en"  }
+  }
+  ORDER BY ?regionLabel ?start`
+}
