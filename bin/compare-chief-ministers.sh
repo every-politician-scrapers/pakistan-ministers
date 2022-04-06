a497ab642c70a55b2ee61a76f9045c0e6f4a0413
Tony Bowden <tony@tmtm.com>
2022-04-06 10:28:21 +0300

    make comparisons less verbose

--- bin/compare-chief-ministers.sh
+++ bin/compare-chief-ministers.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-bundle exec ruby bin/scraper/chief-ministers-wikipedia.rb | ifne tee data/chief-ministers-wikipedia.csv
+bundle exec ruby bin/scraper/chief-ministers-wikipedia.rb > data/chief-ministers-wikipedia.csv
 # TODO: bring WD fetch in-place
 bundle exec ruby bin/diff-chief-ministers.rb | ifne tee data/diff-chief-ministers.csv
 
--- bin/compare-governors.sh
+++ bin/compare-governors.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-bundle exec ruby bin/scraper/governors-wikipedia.rb | ifne tee data/governors-wikipedia.csv
+bundle exec ruby bin/scraper/governors-wikipedia.rb > data/governors-wikipedia.csv
 # TODO: bring WD fetch in-place
 bundle exec ruby bin/diff-governors.rb | ifne tee data/diff-governors.csv
