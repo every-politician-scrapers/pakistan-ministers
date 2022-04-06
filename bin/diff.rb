d3bbeab1a1b7fa566c2724854512c1e4bee146a2
Tony Bowden <tony@tmtm.com>
2022-04-06 10:30:02 +0300

    use null-less comparisons

--- bin/diff.rb
+++ bin/diff.rb
@@ -8,6 +8,6 @@ SKIP = [
   ['---', 'Imran Khan', 'Prime Minister of Pakistan']
 ].freeze
 
-diff = EveryPoliticianScraper::Comparison.new('data/wikidata.csv', 'data/official.csv').diff
-                                         .reject { |row| SKIP.include? row }
+diff = EveryPoliticianScraper::NulllessComparison.new('data/wikidata.csv', 'data/official.csv').diff
+                                                 .reject { |row| SKIP.include? row }
 puts diff.sort_by { |r| [r.first, r[1].to_s] }.reverse.map(&:to_csv)
